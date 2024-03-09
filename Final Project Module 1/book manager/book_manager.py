59fb39303d89a6a112337ba11a4ac917f3a1531b
iuriJorbenadze <ijorb19@freeuni.edu.ge>
2024-03-09 18:09:20 +0400

    Book Class

--- Final Project Module 1/book manager/book_manager.py
+++ Final Project Module 1/book manager/book_manager.py
@@ -21,3 +21,14 @@ class IBookManager(ABC):
     @abstractmethod
     def search_books_by_id(self, book_id):
         pass
+
+
+class Book:
+    def __init__(self, id, title, author, publication_year):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.publication_year = publication_year
+
+    def __str__(self):
+        return f"[ID: {self.id}] '{self.title}' by {self.author}, published in {self.publication_year}"
